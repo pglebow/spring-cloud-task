5c4219795bd666380123902f880651f7e015eb03
Philip Glebow <philip_glebow@gap.com>
2017-02-23 09:28:59 -0800

    setup

---
+++ src/main/java/com/glebow/SpringCloudTaskApplication.java
@@ -0,0 +1,12 @@
+package com.glebow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTaskApplication.class, args);
+	}
+}
